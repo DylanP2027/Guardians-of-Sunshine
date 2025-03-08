a44af59193a50ea69c7e5cb05ab53c6b05846015
DylanP2027 <dkparas@ucsc.edu>
2025-03-07 17:29:26 -0800

    Added death sound effect. Also included some gameplay UI elements

---
+++ assets/src/scenes/Menu.js
@@ -0,0 +1,86 @@
+// Event trigger so that menu stuff isn't triggered more than once
+let enteredMenuScene = false
+
+class Menu extends Phaser.Scene {
+    constructor() {
+        super("menuScene"); // Assigns the key name to this scene
+    }
+
+    preload() {
+        // Load Main Menu spritesheet
+        this.load.spritesheet('mainMenuSpriteSheet', './assets/mainMenu.png', {
+            frameWidth: 1920,
+            frameHeight: 1080,
+            startFrame: 0,
+            endFrame: 1
+        });
+
+        // Load GameOver Screen
+        this.load.image('youLose', './assets/youLose.png');
+        this.load.audio('gameOverSound', './assets/gameOverSound.mp3')
+
+        // Load Win Screen (Fixed the key name)
+        this.load.image('youWin', './assets/youWin.png');
+
+        // Load Current Level Scene
+        this.load.image('currentLevel', './assets/level1.png');
+
+        // Load enemies
+        this.load.image('sleepySam', './assets/sleepySam.png');
+        this.load.image('hunnyBunny', './assets/hunnyBunny.png');
+        this.load.image('bouncyBee', './assets/bouncyBee.png');
+
+        // Load audio
+        this.load.audio('gameMusic', './assets/gameMusic.mp3');
+    }
+
+    create() {
+        if(enteredMenuScene != true) {
+
+            enteredMenuScene = true; // Flags the event
+
+            // Animation for the title screen
+            this.anims.create({
+                key: 'mainMenu',
+                frames: this.anims.generateFrameNumbers('mainMenuSpriteSheet', { start: 0, end: 1, first: 0 }),
+                frameRate: 1,
+                repeat: -1
+            });
+
+            // Background Music
+            this.gameMusic = this.sound.add('gameMusic');
+            this.gameMusic.volume = 0.1;
+            this.gameMusic.loop = true;
+            this.gameMusic.play();
+            this.registry.set('gameMusic', this.gameMusic); // Store in registry
+        }
+
+        // Create the main menu sprite and position it at the center
+        this.mainMenuSprite = this.add.sprite(this.scale.width / 2, this.scale.height / 2, 'mainMenuSpriteSheet');
+       
+        // Set the sprite's origin to the center
+        this.mainMenuSprite.setOrigin(0.5, 0.5);
+
+        // Scale to fit the height of the screen while maintaining aspect ratio
+        let scaleFactor = this.scale.height / this.mainMenuSprite.height;
+        this.mainMenuSprite.setScale(scaleFactor);
+
+        // Play the animation
+        this.mainMenuSprite.play('mainMenu');
+
+        // Define keys for menu navigation
+        this.keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+        this.keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+    }
+
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(this.keyUP)) {
+            this.scene.start('currentLevelScene');
+            // this.menuSelectionSound.play();
+        }
+        if (Phaser.Input.Keyboard.JustDown(this.keyDOWN)) {
+            this.scene.start('gameOverScene');
+            // this.menuSelectionSound.play();
+        }
+    }
+}
---
+++ assets/src/scenes/Win.js
@@ -0,0 +1,23 @@
+class Win extends Phaser.Scene {
+    constructor() {
+        super("winScene") // Basically gives names the key to this object menuScene
+    }
+
+    create() {
+        // Background Color (Black)
+        this.add.rectangle(0, 0, game.config.width, game.config.height, 0x000000).setOrigin(0,0)
+
+        // You Win Sprite
+        this.youWinSprite = this.add.sprite(this.scale.width / 2, this.scale.height / 2, 'youWin').setScale(8);
+
+        // Defines control for this scene
+        keyRESET = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R)
+    }
+
+    update() {
+        if (Phaser.Input.Keyboard.JustDown(keyRESET)) {
+            this.scene.start('menuScene') // Returns to the menu
+            // this.menuSelectionSoundReturn.play()
+        }
+    }
+}
